9866d6987b471b4f4003c4220a82768a3416f958
VR950612 <veni.ramsamy@gmail.com>
2024-11-14 08:31:32 +0800

    Try deploying now

---
+++ migrations/versions/6375dba0039c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 6375dba0039c
+Revises: 
+Create Date: 2024-11-14 08:29:04.143142
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6375dba0039c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/fa80905c3e5c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fa80905c3e5c
+Revises: 6375dba0039c
+Create Date: 2024-11-14 08:30:55.066718
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fa80905c3e5c'
+down_revision = '6375dba0039c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
